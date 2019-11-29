a443b2ba71c47a7164248712a419020a99b88b5c
douglasmg7 <douglasmg7@gmail.com>
2019-11-29 19:29:40 -0300

    Passing file in and out param to xml-format script

--- xml-format/app.js
+++ xml-format/app.js
@@ -1,13 +1,19 @@
+#!/usr/bin/env node
+'use strict';
+
 const fs = require('fs');
 var pd = require('pretty-data').pd;
+
 // const format = require('xml-formatter');
 // var xml = '<root>content><p xml:space="preserve">This is <b>some</b> content.</content></p>';
  
 // var formattedXml = format(xml);
 // console.log(formattedXml);
-
+let file_in = process.argv[2];
+let file_out = process.argv[3];
 let xml;
-fs.readFile('in.xml', 'utf-8',  (err, data) => {
+
+fs.readFile(file_in, 'utf-8',  (err, data) => {
     if (err) {
         return console.log(err);
     }
@@ -16,8 +22,7 @@ fs.readFile('in.xml', 'utf-8',  (err, data) => {
     let xmlPP = pd.xml(data);
     // xml = format(xml);
 
-
-    fs.writeFile("out.xml", xmlPP, function(err) {
+    fs.writeFile(file_out, xmlPP, function(err) {
         if(err) {
             return console.log(err);
         }
