cebf2444557cde16eb0b5ad3201cd35856f70fe9
douglasmg7 <douglasmg7@gmail.com>
2020-03-04 13:10:51 -0300

    new folder for modules

--- git/status-all.sh
+++ git/status-all.sh
@@ -5,18 +5,18 @@ dirs=`cat << EOF
 /home/douglasmg7/dotfiles
 /home/douglasmg7/txt
 /home/douglasmg7/code/scripts
-/home/douglasmg7/code/zunka
+/home/douglasmg7/code/zunkasite
 /home/douglasmg7/code/cpp-study
 /home/douglasmg7/code/bluetang
 /home/douglasmg7/code/bluewhale
 /home/douglasmg7/code/go-study
-/home/douglasmg7/code/zunka-modules/aldowsc
-/home/douglasmg7/code/zunka-modules/aldoutil
-/home/douglasmg7/code/zunka-modules/currency
-/home/douglasmg7/code/zunka-modules/zunkasrv
-/home/douglasmg7/code/zunka-modules/zoomwsc
-/home/douglasmg7/code/zunka-modules/zoomproducts
-/home/douglasmg7/code/zunka-modules/freightsrv
+/home/douglasmg7/code/aldowsc
+/home/douglasmg7/code/aldoutil
+/home/douglasmg7/code/currency
+/home/douglasmg7/code/zunkasrv
+/home/douglasmg7/code/zoomwsc
+/home/douglasmg7/code/zoomproducts
+/home/douglasmg7/code/freightsrv
 EOF`
 
 for path in $dirs
