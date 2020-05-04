fd2ea336f51f535accffaf0e39c5abef120ba482
douglasmg7 <douglasmg7@gmail.com>
2020-05-04 09:13:07 -0300

    included rust-study on git

--- git/status-all.sh
+++ git/status-all.sh
@@ -5,11 +5,13 @@ dirs=`cat << EOF
 /home/douglasmg7/dotfiles
 /home/douglasmg7/txt
 /home/douglasmg7/code/scripts
-/home/douglasmg7/code/zunkasite
+/home/douglasmg7/code/c
 /home/douglasmg7/code/cpp-study
+/home/douglasmg7/code/go-study
+/home/douglasmg7/code/rust-study
+/home/douglasmg7/code/zunkasite
 /home/douglasmg7/code/bluetang
 /home/douglasmg7/code/bluewhale
-/home/douglasmg7/code/go-study
 /home/douglasmg7/code/aldowsc
 /home/douglasmg7/code/aldoutil
 /home/douglasmg7/code/currency
