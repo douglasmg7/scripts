b847241c6af1798a830a3cb3e9fdfa564d920730
douglasmg7 <douglasmg7@gmail.com>
2021-03-30 13:13:16 -0300

    send HDM1 monitor to above

--- monitor/monitor_config.sh
+++ monitor/monitor_config.sh
@@ -3,4 +3,6 @@
 # Set resolution for hdm1.
 xrandr --output HDMI1 --mode 1920x1080
 # Set monitor HDMI1 to left of main monitor.
-xrandr --output eDP1 --auto --output HDMI1 --left-of eDP1
+# xrandr --output eDP1 --auto --output HDMI1 --left-of eDP1
+# xrandr --output eDP1 --auto --output HDMI1 --right-of eDP1
+xrandr --output eDP1 --auto --output HDMI1 --above eDP1
