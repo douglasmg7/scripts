4cb0f3d6f1a292e18e47776b0864cbb3596b6dc8
douglasmg7 <douglasmg7@gmail.com>
2021-10-17 13:48:15 -0300

    added motospeed to git status all

--- git/status-all.sh
+++ git/status-all.sh
@@ -30,6 +30,7 @@ dirs=`cat << EOF
 /home/douglasmg7/code/data_science
 /home/douglasmg7/code/meli_products
 /home/douglasmg7/code/handytech
+/home/douglasmg7/code/motospeed
 EOF`
 
 for path in $dirs
