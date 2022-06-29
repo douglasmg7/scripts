fe1ebcc0f8016e72eaede4b84003b395cd75bce7
douglasmg7 <douglasmg7@gmail.com>
2022-06-29 17:40:02 -0300

    added dropify repository

--- git/status-all.sh
+++ git/status-all.sh
@@ -31,6 +31,7 @@ dirs=`cat << EOF
 /home/douglasmg7/code/meli_products
 /home/douglasmg7/code/handytech
 /home/douglasmg7/code/motospeed
+/home/douglasmg7/code/dropify
 /home/douglasmg7/boat/nautica
 EOF`
 
