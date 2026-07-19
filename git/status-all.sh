f3dcdf97cd333007e21ae58b894736aa72fe678d
douglasmg7 <douglasmg7@gmail.com>
2026-07-19 12:46:11 -0300

    added python_the_pratical_guide repository to git sa

--- git/status-all.sh
+++ git/status-all.sh
@@ -26,6 +26,7 @@ dirs=`cat << EOF
 /home/douglasmg7/code/go-study
 /home/douglasmg7/code/microservices_with_nodejs_and_react
 /home/douglasmg7/code/nodejs_the_complete_guide
+/home/douglasmg7/code/python_the_pratical_guide
 EOF`
 
 for path in $dirs
